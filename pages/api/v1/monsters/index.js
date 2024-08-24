98a72e50749f3774273c413b48f932872644ec54
Varela <55703371+Zeitgeist195@users.noreply.github.com>
2024-08-23 22:19:03 -0300

    `monsters` endpoint done

--- pages/api/v1/monsters/index.js
+++ pages/api/v1/monsters/index.js
@@ -1,23 +1,60 @@
 import database from "infra/database.js";
 
+function getLiteralMonsterClass(classId) {
+  let className;
+
+  switch (classId) {
+    default:
+    case "0":
+      className = "Large";
+      break;
+    case "1":
+      className = "Small";
+      break;
+    case "2":
+      className = "Deviant";
+      break;
+  }
+
+  return className;
+}
+
 async function monsters(req, res) {
   const dbResponse = await database.query("SELECT * FROM monsters;");
 
-  const monstersRes = [];
+  const large = [];
+  const deviant = [];
+  const small = [];
 
   dbResponse.rows.forEach((monster) => {
     const monsterObject = {
       _id: monster._id,
-      class: monster.class,
+      classId: monster.class,
+      className: getLiteralMonsterClass(monster.class),
       name: monster.name,
       icon_name: monster.icon_name,
     };
 
-    monstersRes.push(monsterObject);
+    switch (monsterObject.classId) {
+      default:
+      case "0":
+        large.push(monsterObject);
+        break;
+      case "1":
+        small.push(monsterObject);
+        break;
+      case "2":
+        deviant.push(monsterObject);
+        break;
+    }
   });
 
   res.status(200).json({
-    monsters: monstersRes,
+    monsters: {
+      Large: large,
+      Deviant: deviant,
+      Small: small,
+    },
   });
 }
 
