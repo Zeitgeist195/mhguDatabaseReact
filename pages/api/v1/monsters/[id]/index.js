e9a2a61dce0844a998f3590c1c82aafeee4a68b9
Zeitgeist <55703371+Zeitgeist195@users.noreply.github.com>
2024-08-24 20:43:13 -0300

    `monsters/[id]` complete

--- pages/api/v1/monsters/[id]/index.js
+++ pages/api/v1/monsters/[id]/index.js
@@ -77,31 +77,27 @@ async function getAilments(monsterId) {
   return ailments;
 }
 
-async function getLocation(locationId) {
-  const locationDb = await database.query({
-    text: "SELECT * FROM locations WHERE _id = $1;",
-    values: [locationId],
-  });
-
-  return locationDb.rows[0];
-}
-
 async function getHabitats(monsterId) {
-  const habitats = [];
+  let query = "SELECT ";
+  query += "a.*, b.name, b.map ";
+  query += "FROM monster_habitat ";
+  query += "AS a INNER JOIN locations AS b ON a.location_id = b._id ";
+  query += "WHERE a.monster_id = $1 GROUP BY (a._id, b.name, b.map, b._id) ";
+  query += "ORDER BY b._id;";
+  const habitats = {};
   const habitatsDb = await database.query({
-    text: "SELECT * FROM monster_habitat WHERE monster_id = $1;",
+    text: query,
     values: [monsterId],
   });
 
   for (const habitat of habitatsDb.rows) {
-    const habitatObj = {
-      location: await getLocation(habitat.location_id),
+    habitats[habitat._id] = {
+      locationName: habitat.name,
+      locationMap: habitat.map,
       startArea: habitat.start_area,
       moveArea: habitat.move_area,
       restArea: habitat.rest_area === "NaN" ? "0" : habitat.rest_area.toString().replace(".0", ""),
     };
-
-    habitats.push(habitatObj);
   }
 
   return habitats;
@@ -143,34 +139,87 @@ async function getDamage(monsterId) {
   return bodyDmg;
 }
 
-async function getRewards(monsterId, rank) {
-  const damagesDb = await database.query({
-    text: "SELECT * FROM hunting_rewards WHERE (monster_id = $1 AND rank = $2);",
-    values: [monsterId, rank],
+async function getRewards(monsterId) {
+  let query = "SELECT ";
+  query += "a.*, b.name, b.icon_name, b.icon_color ";
+  query += "FROM hunting_rewards ";
+  query += "AS a INNER JOIN items AS b ON a.item_id = b._id ";
+  query +=
+    "WHERE a.monster_id = $1 GROUP BY (a.condition, a._id, b.name, b.icon_name, icon_color) ";
+  query += "ORDER BY a.rank DESC, _id;";
+  // "SELECT a.*, b.name, b.map FROM monster_location AS a INNER JOIN locations AS b ON a.location_id = b._id WHERE a.monster_id = 60 GROUP BY (a.condition, b.name, b.map) ORDER BY b._id;";
+  const rewardsDb = await database.query({
+    text: query,
+    values: [monsterId],
   });
 
-  const bodyDmg = {};
+  const rewards = {};
 
-  for (const damage of damagesDb.rows) {
-    for (var prop in damage) {
-      if (Object.prototype.hasOwnProperty.call(damage, prop)) {
-        if (physicalDefinition.includes(prop)) {
-          physicalDmg[prop] = damage[prop];
-        } else if (elementalDefinition.includes(prop)) {
-          elementalDmg[prop] = damage[prop];
-        }
-      }
+  for (const reward of rewardsDb.rows) {
+    if (!rewards.hasOwnProperty(reward.rank)) {
+      rewards[reward.rank] = {};
+    }
+    if (!rewards[reward.rank].hasOwnProperty(reward.condition)) {
+      rewards[reward.rank][reward.condition] = {};
     }
 
-    const damageObj = {
-      physical: physicalDmg,
-      elemental: elementalDmg,
+    rewards[reward.rank][reward.condition][reward.item_id] = {
+      itemName: reward.name,
+      itemIcon: reward.icon_name,
+      itemColor: reward.icon_color,
+      stackSize: reward.stack_size,
+      percentage: reward.percentage,
     };
-
-    bodyDmg[damage.body_part] = damageObj;
   }
 
-  return bodyDmg;
+  return rewards;
+}
+
+async function getMonsterQuests(monsterId) {
+  let query = "SELECT ";
+  query += "a.*, b.name, b.stars, b.rank, b.hub ";
+  query += "FROM monster_to_quest ";
+  query += "AS a INNER JOIN quests AS b ON a.quest_id = b._id ";
+  query += "WHERE a.monster_id = $1 GROUP BY (b.hub, a.quest_id, b.stars, b.rank, b.name, a._id) ";
+  query += "ORDER BY a.quest_id;";
+
+  const mtqDb = await database.query({
+    text: query,
+    values: [monsterId],
+  });
+
+  const quests = {};
+
+  for (const quest of mtqDb.rows) {
+    if (!quests.hasOwnProperty(quest.hub)) {
+      quests[quest.hub] = {};
+    }
+
+    quests[quest.hub][quest.quest_id] = {
+      questName: quest.name,
+      questStars: quest.stars,
+      isUnstable: quest.unstable === 1 ? true : false,
+      isHyper: quest.hyper === 1 ? true : false,
+    };
+  }
+
+  return quests;
+}
+
+function getMonsterClass(classId) {
+  let literalClass = "";
+  switch (classId) {
+    default:
+    case "0":
+      literalClass = "Large";
+      break;
+    case "1":
+      literalClass = "Small";
+      break;
+    case "2":
+      literalClass = "Deviant";
+      break;
+  }
 }
 
 export default async function monster(req, res) {
@@ -181,19 +230,24 @@ export default async function monster(req, res) {
     values: [id],
   });
 
+  const base = {
+    id: id,
+    class: getMonsterClass(id),
+    name: monsterBase.rows[0].name,
+    iconName: monsterBase.rows[0].icon_name,
+    sortName: monsterBase.rows[0].sort_name,
+    baseHp: monsterBase.rows[0].base_hp,
+  };
+
   res.status(200).json({
     summary: {
-      base: monsterBase.rows[0],
+      base: base,
       states: await getWeaknesses(id),
       ailments: await getAilments(id),
       habitats: await getHabitats(id),
     },
     damage: await getDamage(id),
-    huntingRewards: {
-      LRDrops: "",
-      HRDrops: "",
-      GDrops: "",
-    },
-    quests: "",
+    huntingRewards: await getRewards(id),
+    quests: await getMonsterQuests(id),
   });
 }
